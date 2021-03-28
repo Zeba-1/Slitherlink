a16d327240b3de521009c4c51ea3b06c2eb11d8d
Zeba-1 <sebsautier18@gmail.com>
2021-03-28 15:59:14 +0200

    first function and graphics

--- slitherlink.py
+++ slitherlink.py
@@ -1,6 +1,10 @@
 # Slitherlink for UGE-AP2 by Zeba & Dwarzen in Python3
 # Subject: https://elearning.u-pem.fr/pluginfile.php/511264/mod_resource/content/3/projet.pdf
 
+import fltk as fl
+
+TAILLE_CASE = 150
+TAILLE_MARGE = 50
 
 # fonction pour gerer les segments
 
@@ -84,6 +88,21 @@ def cree_grille(fichier):
     transforme le contenue d'un fichier txt en liste de liste
     pour former le plateau de jeu !
     """
+    grilleTxt = open(fichier, "r")
+    indices = []
+
+    i = 0
+    for ligne in grilleTxt:
+        indices.append([])
+        for char in ligne.rstrip("\n"):
+            if char == "_":
+                indices[i].append(None)
+            else:
+                indices[i].append(char)
+        i += 1
+
+    grilleTxt.close()
+    return indices
 
 # fonction graphique !
 
@@ -94,6 +113,23 @@ def dessine_plateau(indices, marge, tailleCase):
     Cette foncion recrer un fenetre avec une taille choisie en
     fonction du tableau indices.
     """
+    fl.cree_fenetre(len(indices[0]) * tailleCase + marge*2,
+                    len(indices) * tailleCase + marge*2)
+
+    # boucle pour le dessin des points
+    for x in range(len(indices[0])+1):
+        for y in range(len(indices)+1):
+            fl.cercle(marge+(x*tailleCase), marge+(y*tailleCase), 10,
+                      remplissage='black')
+
+    # boucle pour le dessin des indices
+    for x, ligne in enumerate(indices):
+        for y, indice in enumerate(ligne):
+            if indice == None: continue
+            fl.texte(marge+(tailleCase/2) + y*tailleCase,
+                     marge+(tailleCase/2) + x*tailleCase,
+                     indice, ancrage='center')
+            
 
 
 def dessine_segment(etat, marge, tailleCase):
@@ -104,4 +140,6 @@ def dessine_segment(etat, marge, tailleCase):
 
 # programme principal
 if __name__ == '__main__':
-    pass
+    indices = cree_grille("test.txt")
+    dessine_plateau(indices, TAILLE_MARGE, TAILLE_CASE)
+    fl.attend_fermeture()
